241ce8713825c1b30013e3d1f9cfe1a3d63f855e
eleven12 <maddinenichenchunaidu@gmail.com>
2021-08-19 21:53:51 +0530

    added carousel component

---
+++ src/component/Carousel.jsx
@@ -0,0 +1,32 @@
+import React,{useState} from 'react'
+import {images} from './data'
+import './carousel.css'
+import {AiOutlineArrowLeft,AiOutlineArrowRight} from 'react-icons/ai'
+
+export default function Carousel() {
+    const [currentImage,setCurrentImage] = useState(0)
+    const handleImageChange = (index)=>{
+        if(index<images.length && index>=0){
+            setCurrentImage(index)
+        }
+    }
+    return (
+        <div>
+            <div className='carousel-container'>
+                <button onClick={()=>handleImageChange(currentImage-1)}>
+                    <AiOutlineArrowLeft size="36px" color="#4CAF50"/>
+                </button>
+                <img src={images[currentImage]} alt={`${currentImage+1}`} width="70%" height="70%"/>
+                <button onClick={()=>handleImageChange(currentImage+1)}>
+                    <AiOutlineArrowRight size="36px" color="#4CAF50"/>
+                </button>
+            </div>
+            <div className="circles-container">
+                {images.map((key,index)=>{
+                    return <span className={`${currentImage===index?"circle-active":"circle"}`} onClick={()=>handleImageChange(index)}>{index+1}</span>   
+                })}
+            </div>
+        </div>
+        
+    )
+}
---
+++ src/component/data.js
@@ -0,0 +1,6 @@
+export const images = [
+    "https://images.unsplash.com/photo-1588948378271-ae6cf0060ff8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1628087979081-53f35b610ffe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1628148833282-023019fd39bd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1626731272038-2d6821366049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80"
+]
